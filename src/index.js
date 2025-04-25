4c9c666ef844bb9367cda0efdccc1799078b6e20
TheMan-Umar <themanumar1@gmail.com>
2025-04-25 09:35:58 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'tachyons';
 import reportWebVitals from './reportWebVitals';
+import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App robots={robots} /> 
   </React.StrictMode>
 );
 
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Sani Khaliboy',
+    username: 'Khaliboy',
+    email: 'Khaliboy@Rigachikun'
+  },
+  {
+    id: 2,
+    name: 'Abdullahi Maye',
+    username: 'Maye',
+    email: 'Maye@U/dosa'
+  },
+  {
+    id: 3,
+    name: 'Muh\'d Dangama',
+    username: 'Dangama',
+    email: 'Dangama@Tudun Wada'
+  },
+  {
+    id: 4,
+    name: 'Adam Investor',
+    username: 'Investor',
+    email: 'Investor@U/dosa'
+  },
+  {
+    id: 5,
+    name: 'Kabir Gawuna',
+    username: 'Gawuna',
+    email: 'Kabir@kabala'
+  },
+  {
+    id: 6,
+    name: 'Muhammad Pogba',
+    username: 'Pogba',
+    email: 'Muhammad@Tudun Wada'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
